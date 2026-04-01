42c917ccc49f7750633d0c421b218e2dd9818193
Mahesh <gmaheshreddy538@gmail.com>
2026-04-01 15:42:12 +0530

    add routing and create Body and Login components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
 
 function App() {
 
   return (
-    <>
-      <Navbar />
-      <Footer />
-    </>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
