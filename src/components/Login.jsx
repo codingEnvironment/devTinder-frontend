23a4d75390090aa3de6d07635aa6d0cfbf8d3c7b
Mahesh <gmaheshreddy538@gmail.com>
2026-04-03 16:44:14 +0530

    add axios for login functionality and update Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,35 @@
-import {useRef} from 'react';
+import { useRef } from 'react';
+import axios from 'axios';
 
 const Login = () => {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleClick = async () => {
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
     console.log('Email:', email);
     console.log('Password:', password);
+    try {
+      // { withCredentials: true } is required so that cookies will get saved in browser and sent with subsequent requests
+      const response = await axios.post('http://localhost:3000/login', { email, password }, { withCredentials: true });
+      console.log('Login successful:', response.data);
+    } catch (error) {
+      console.error('Login failed:', error.response.data);
+    }
   };
 
   return (
-    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4" style={{margin: '100px auto'}}>
+    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4" style={{ margin: '100px auto' }}>
       <legend className="fieldset-legend">Login</legend>
 
       <label className="label">Email</label>
-      <input type="email" className="input" placeholder="Email" ref={emailRef} />
+      <input type="email" className="input" placeholder="Email" ref={emailRef} defaultValue="mahesh@gmail.com" />
 
       <label className="label">Password</label>
-      <input type="password" className="input" placeholder="Password" ref={passwordRef} />
+      <input type="password" className="input" placeholder="Password" ref={passwordRef} defaultValue="m1234" />
 
-      <button className="btn btn-neutral mt-4" onClick={handleSubmit}>
+      <button className="btn btn-neutral mt-4" onClick={handleClick}>
         Login
       </button>
     </fieldset>
