2594dfffe4b046c2ef3beaa036d1b80fb2b6ac56
Mahesh <gmaheshreddy538@gmail.com>
2026-04-08 17:28:27 +0530

    re-tain the store on page load

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import axios from "axios";
+import { API_BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUserDetails = async () => {
+    try {
+      if (user) return; // If user details are already present in the store, no need to fetch again
+      const userDetails = await axios.get(API_BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(userDetails.data));
+    } catch (error) {
+      console.error("Error fetching user details:", error.status);
+      if (error?.status === 401) {
+        navigate("/login"); // Redirect to login page if fetching user details fails
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+
   return (
     <>
       <Navbar />
